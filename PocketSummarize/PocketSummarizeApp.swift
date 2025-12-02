fefc52f7a36a70d82d10ac704d21377d34355c40
Abhishek Kurmi <itsabhi814@gmail.com>
2025-12-02 11:22:01 +0530

    Initial Commit

---
+++ PocketSummarize/PocketSummarizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PocketSummarizeApp.swift
+//  PocketSummarize
+//
+//  Created by Apple on 02/12/25.
+//
+
+import SwiftUI
+
+@main
+struct PocketSummarizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
